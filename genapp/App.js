3e921eaa6197c75f262cc02782e2b7800e827b9f
MarioWizRaccoon <mlafleur@lanla.com>
2019-03-16 10:21:32 -0400

    clean up unnecessary import

--- genapp/App.js
+++ genapp/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 import {White} from './constant/color';
 
